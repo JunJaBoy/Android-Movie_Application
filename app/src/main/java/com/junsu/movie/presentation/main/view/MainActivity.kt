9f27529be3b56283b56f0110956fc87481636c0b
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 13:47:54 +0900

    refactor :: MainActivity

--- app/src/main/java/com/junsu/movie/presentation/main/view/MainActivity.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/view/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.junsu.movie.presentation.main.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.junsu.movie.R
+import androidx.appcompat.app.AppCompatActivity
+import com.junsu.movieapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
