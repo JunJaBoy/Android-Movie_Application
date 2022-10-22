c088b154f99694a439d07df7af0c4871f6589770
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 16:34:02 +0900

    feat :: set MyPageFragment extends BaseFragment

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/view/MyPageFragment.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/view/MyPageFragment.kt
@@ -1,4 +1,11 @@
 package com.junsu.movie.presentation.main.fragment.mypage.view
 
-class MyPageFragment {
+import com.junsu.movie.presentation.base.BaseFragment
+import com.junsu.movieapplication.R
+import com.junsu.movieapplication.databinding.FragmentMypageBinding
+
+class MyPageFragment : BaseFragment<FragmentMypageBinding>(
+    R.layout.fragment_mypage
+) {
+    override fun observeEvent() {}
 }
