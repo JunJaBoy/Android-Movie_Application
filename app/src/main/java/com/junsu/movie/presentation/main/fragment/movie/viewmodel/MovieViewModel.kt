df81827ef132bfc64d301ebc3120604d716e22b7
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 19:36:43 +0900

    temp

---
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/viewmodel/MovieViewModel.kt
@@ -0,0 +1,7 @@
+package com.junsu.movie.presentation.main.fragment.movie.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.junsu.movie.data.repository.main.MainRepository
+
+class MovieViewModel(private val mainRepository: MainRepository) : ViewModel() {
+}
