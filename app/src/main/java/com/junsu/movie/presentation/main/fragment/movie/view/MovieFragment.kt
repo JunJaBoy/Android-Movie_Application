faba23f1200a39334b04f89c616e1cf681f920a9
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 19:36:40 +0900

    temp

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
@@ -5,18 +5,29 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.junsu.movie.data.api.MovieService
 import com.junsu.movie.presentation.base.BaseFragment
+import com.junsu.movie.presentation.main.fragment.movie.adapter.DailyBoxOfficeAdapter
 import com.junsu.movieapplication.R
 import com.junsu.movieapplication.databinding.FragmentMovieBinding
 
+// 순위에 따라 금, 은, 동색 순위
 class MovieFragment(
     override var parentActivity: AppCompatActivity
-) :
-    BaseFragment<FragmentMovieBinding>(
-        R.layout.fragment_movie
-    ) {
+) : BaseFragment<FragmentMovieBinding>(
+    R.layout.fragment_movie
+) {
+
+
+    private lateinit var dailyBoxOfficeAdapter: DailyBoxOfficeAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        dailyBoxOfficeAdapter = DailyBoxOfficeAdapter()
+        binding.rvMovieDaily.adapter = dailyBoxOfficeAdapter
+
+
+
 
         getDailyBoxOffice()
     }
