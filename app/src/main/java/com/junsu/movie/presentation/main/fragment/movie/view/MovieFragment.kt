49ee5249273ca17af8d3aeebd8c0815e670b3a37
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 16:40:21 +0900

    feat :: movieFragment parentActivity

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
@@ -1,20 +1,25 @@
 package com.junsu.movie.presentation.main.fragment.movie.view
 
-import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.junsu.movie.data.api.MovieService
 import com.junsu.movie.data.api.RetrofitClient
 import com.junsu.movie.presentation.base.BaseFragment
 import com.junsu.movieapplication.R
 import com.junsu.movieapplication.databinding.FragmentMovieBinding
 
-class MovieFragment : BaseFragment<FragmentMovieBinding>(
-    R.layout.fragment_movie
-) {
-    //        getDailyBoxOffice()
+class MovieFragment(override var parentActivity: AppCompatActivity) :
+    BaseFragment<FragmentMovieBinding>(
+        R.layout.fragment_movie
+    ) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
+        Toast.makeText(parentActivity, "", Toast.LENGTH_SHORT).show()
+        getDailyBoxOffice()
     }
 
     fun getDailyBoxOffice() {
