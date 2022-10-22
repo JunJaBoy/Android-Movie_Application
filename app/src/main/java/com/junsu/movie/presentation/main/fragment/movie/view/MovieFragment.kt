60bed5ba55040e463b5ceebc95149a432c77a2f7
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 16:35:14 +0900

    feat :: MovieFragment Data Receiving

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
@@ -1,5 +1,8 @@
 package com.junsu.movie.presentation.main.fragment.movie.view
 
+import android.content.Context
+import com.junsu.movie.data.api.MovieService
+import com.junsu.movie.data.api.RetrofitClient
 import com.junsu.movie.presentation.base.BaseFragment
 import com.junsu.movieapplication.R
 import com.junsu.movieapplication.databinding.FragmentMovieBinding
@@ -8,4 +11,18 @@ class MovieFragment : BaseFragment<FragmentMovieBinding>(
     R.layout.fragment_movie
 ) {
     //        getDailyBoxOffice()
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    fun getDailyBoxOffice() {
+        val rt = RetrofitClient.getRetrofitClient()
+        val movieService = MovieService()
+        movieService.getDailyBoxOfficeList("20221020")
+        println(movieService.toString())
+    }
+
+    override fun observeEvent() {}
 }
