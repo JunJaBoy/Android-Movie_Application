2df582197a026191e65b18c9dfdd26bddadaccf2
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 23:53:59 +0900

    feat :: movie API

--- app/src/main/java/com/junsu/movie/data/api/MovieAPI.kt
+++ app/src/main/java/com/junsu/movie/data/api/MovieAPI.kt
@@ -1,15 +1,22 @@
 package com.junsu.movie.data.api
 
 import com.junsu.movie.data.model.DailyBoxOfficeResponse
-import retrofit2.Call
+import com.junsu.movie.data.model.WeeklyBoxOfficeResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MovieAPI {
+
     @GET("boxoffice/searchDailyBoxOfficeList.json")
     suspend fun getDailyBoxOfficeList(
         @Query("key") key: String,
         @Query("targetDt") targetDt: String,
     ): Response<DailyBoxOfficeResponse>
+
+    @GET("boxoffice/boxoffice/searchWeeklyBoxOfficeList.json")
+    suspend fun getWeeklyBoxOfficeList(
+        @Query("key") key: String,
+        @Query("targetDt") targetDt: String,
+    ): Response<WeeklyBoxOfficeResponse>
 }
