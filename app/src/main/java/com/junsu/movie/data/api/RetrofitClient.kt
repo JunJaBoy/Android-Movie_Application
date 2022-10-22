e152d6ff6ec95b8926818a04bf5b097000b0a81e
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 14:11:59 +0900

    feat :: RetrofitClient

--- app/src/main/java/com/junsu/movie/data/api/RetrofitClient.kt
+++ app/src/main/java/com/junsu/movie/data/api/RetrofitClient.kt
@@ -1,5 +1,23 @@
 package com.junsu.movie.data.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitClient {
-    private const val BASE_URL = ""
+    private const val BASE_URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest" // + type
+    private const val API_KEY = "deca283822d954110693a73fcee8f480"
+
+    private var retrofit: Retrofit? = null
+
+    fun getInstance(type: String): Retrofit? {
+        return retrofit ?: let {
+            Retrofit.Builder()
+                .baseUrl(BASE_URL + type)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
 }
+
+const val BOX_OFFICE = "/boxoffice"
+const val MOVIE = "/movie"
