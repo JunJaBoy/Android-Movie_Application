04227de09dfaef23e992c01deab5e5183581aa7f
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 16:26:40 +0900

    feat :: BaseFragment

--- app/src/main/java/com/junsu/movie/presentation/base/BaseFragment.java
+++
@@ -1,4 +0,0 @@
-package com.junsu.movie.presentation.base;
-
-public interface BaseFragment {
-}
--- app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
@@ -1,4 +1,11 @@
 package com.junsu.movie.presentation.main.fragment.movie.view
 
-class MovieFragment {
+import com.junsu.movie.presentation.base.BaseFragment
+import com.junsu.movieapplication.R
+import com.junsu.movieapplication.databinding.FragmentMovieBinding
+
+class MovieFragment : BaseFragment<FragmentMovieBinding>(
+    R.layout.fragment_movie
+) {
+    //        getDailyBoxOffice()
 }
