15707b11a57fdbc02c9aaefe8fb903c448535e65
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 16:40:34 +0900

    feat :: implement parentActivity in MyPageFragment

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/view/MyPageFragment.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/view/MyPageFragment.kt
@@ -1,11 +1,17 @@
 package com.junsu.movie.presentation.main.fragment.mypage.view
 
+import androidx.appcompat.app.AppCompatActivity
 import com.junsu.movie.presentation.base.BaseFragment
 import com.junsu.movieapplication.R
 import com.junsu.movieapplication.databinding.FragmentMypageBinding
 
-class MyPageFragment : BaseFragment<FragmentMypageBinding>(
-    R.layout.fragment_mypage
-) {
+class MyPageFragment(
+    override var parentActivity: AppCompatActivity
+) :
+    BaseFragment<FragmentMypageBinding>(
+        R.layout.fragment_mypage
+    ) {
+
+
     override fun observeEvent() {}
 }
