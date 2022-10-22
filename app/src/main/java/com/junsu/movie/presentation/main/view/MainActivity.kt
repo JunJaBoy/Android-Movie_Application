8a6f11a58fdb1d92c92c4c063ac6c0e84e383f75
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 17:04:53 +0900

    feat :: fragment change logic in MainActivity

--- app/src/main/java/com/junsu/movie/presentation/main/view/MainActivity.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/view/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.junsu.movie.presentation.main.view
 
 import android.os.Bundle
-import com.junsu.movie.data.api.MovieService
-import com.junsu.movie.data.api.RetrofitClient
+import androidx.fragment.app.Fragment
 import com.junsu.movie.presentation.base.BaseActivity
+import com.junsu.movie.presentation.main.fragment.movie.view.MovieFragment
+import com.junsu.movie.presentation.main.fragment.mypage.view.MyPageFragment
 import com.junsu.movieapplication.R
 import com.junsu.movieapplication.databinding.ActivityMainBinding
 
@@ -11,8 +12,43 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
     R.layout.activity_main
 ) {
 
+    private val movieFragment by lazy {
+        MovieFragment(this)
+    }
+
+    private val myPageFragment by lazy {
+        MyPageFragment(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
+        with(binding.bnMain) {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.menu_bn_main_movie -> {
+                        changeFragment(movieFragment)
+                    }
+                    R.id.menu_bn_main_myPage -> {
+                        changeFragment(myPageFragment)
+                    }
+                    else -> {
+                        false
+                    }
+                }
+            }
+            selectedItemId = R.id.menu_bn_main_movie
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment): Boolean {
+        supportFragmentManager.beginTransaction().replace(R.id.frame_main, fragment)
+            .commitAllowingStateLoss()
+        return true
     }
 
     override fun observeEvent() {
