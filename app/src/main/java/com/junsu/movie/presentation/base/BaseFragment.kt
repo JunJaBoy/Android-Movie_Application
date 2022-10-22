fd312fdd7945e0fd4b24ca34fd36dc6bf2752869
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 16:40:04 +0900

    feat :: parentActivity instance in BaseFragment

--- app/src/main/java/com/junsu/movie/presentation/base/BaseFragment.kt
+++ app/src/main/java/com/junsu/movie/presentation/base/BaseFragment.kt
@@ -1,10 +1,12 @@
 package com.junsu.movie.presentation.base
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
@@ -13,7 +15,15 @@ abstract class BaseFragment<B : ViewDataBinding>(
     @LayoutRes private val layoutRes: Int,
 ) : Fragment() {
 
+    abstract var parentActivity: AppCompatActivity
+
     private lateinit var binding: B
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        parentActivity = context as AppCompatActivity
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
