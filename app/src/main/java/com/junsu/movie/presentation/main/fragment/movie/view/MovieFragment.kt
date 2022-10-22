b32d7a64367af2b7d3dafe1d5eb5ff8955696f77
JunsuMacbook <krpjs0508@gmail.com>
2022-10-22 17:21:59 +0900

    refactor :: getDailyBoxOffice accessor

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.junsu.movie.data.api.MovieService
-import com.junsu.movie.data.api.RetrofitClient
 import com.junsu.movie.presentation.base.BaseFragment
 import com.junsu.movieapplication.R
 import com.junsu.movieapplication.databinding.FragmentMovieBinding
@@ -22,8 +21,7 @@ class MovieFragment(
         getDailyBoxOffice()
     }
 
-    fun getDailyBoxOffice() {
-        val rt = RetrofitClient.getRetrofitClient()
+    private fun getDailyBoxOffice() {
         val movieService = MovieService()
         movieService.getDailyBoxOfficeList("20221020")
         println(movieService.toString())
