ca10478ab978e862c238831ea1760916de93159c
JunsuMacbook <krpjs0508@gmail.com>
2022-10-23 16:27:06 +0900

    refactor :: movieApi

--- app/src/main/java/com/junsu/movie/data/api/MovieAPI.kt
+++ app/src/main/java/com/junsu/movie/data/api/MovieAPI.kt
@@ -1,6 +1,7 @@
 package com.junsu.movie.data.api
 
 import com.junsu.movie.data.model.DailyBoxOfficeResponse
+import com.junsu.movie.data.model.WeeklyBoxOfficeList
 import com.junsu.movie.data.model.WeeklyBoxOfficeResponse
 import retrofit2.Response
 import retrofit2.http.GET
