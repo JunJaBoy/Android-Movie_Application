3beb880b500abc95ec136c288e060b2feabbddcd
JunsuMacbook <krpjs0508@gmail.com>
2022-10-23 15:40:12 +0900

    feat :: define logic

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/view/MovieFragment.kt
@@ -2,9 +2,7 @@ package com.junsu.movie.presentation.main.fragment.movie.view
 
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.junsu.movie.common.util.getDummyDate
 import com.junsu.movie.data.repository.main.MovieRepository
 import com.junsu.movie.presentation.base.BaseFragment
 import com.junsu.movie.presentation.main.fragment.movie.adapter.DailyBoxOfficeAdapter
@@ -13,10 +11,10 @@ import com.junsu.movie.presentation.main.fragment.movie.viewmodel.MovieViewModel
 import com.junsu.movieapplication.R
 import com.junsu.movieapplication.databinding.FragmentMovieBinding
 
-// 순위에 따라 금, 은, 동색 순위
-class MovieFragment: BaseFragment<FragmentMovieBinding>(
+class MovieFragment : BaseFragment<FragmentMovieBinding>(
     R.layout.fragment_movie
 ) {
+
     private val viewModel by lazy {
         ViewModelProvider(
             this,
@@ -31,14 +29,8 @@ class MovieFragment: BaseFragment<FragmentMovieBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initBoxOffice()
-
         binding.rvMovieDaily.adapter = dailyBoxOfficeAdapter
         observeDailyBoxOffice()
-    }
-
-    private fun initBoxOffice() {
-        viewModel.getDailyBoxOffice(getDummyDate())
     }
 
     private fun observeDailyBoxOffice() {
