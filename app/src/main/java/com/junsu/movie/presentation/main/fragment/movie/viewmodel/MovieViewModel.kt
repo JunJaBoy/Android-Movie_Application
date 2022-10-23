8951cdc4468134471291149f5bd274b8fdcad548
JunsuMacbook <krpjs0508@gmail.com>
2022-10-23 14:11:25 +0900

    feat :: add coroutines in MovieViewModel functions

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/movie/viewmodel/MovieViewModel.kt
@@ -1,10 +1,12 @@
 package com.junsu.movie.presentation.main.fragment.movie.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.junsu.movie.data.model.DailyBoxOfficeResponse
+import com.junsu.movie.data.model.WeeklyBoxOfficeResponse
 import com.junsu.movie.data.repository.main.MovieRepository
 import kotlinx.coroutines.launch
 import retrofit2.Response
@@ -14,9 +16,30 @@ class MovieViewModel(private val repository: MovieRepository) : ViewModel() {
     private var _dailyBoxOfficeMovies = MutableLiveData<Response<DailyBoxOfficeResponse>>()
     val dailyBoxOfficeMovies: LiveData<Response<DailyBoxOfficeResponse>> = _dailyBoxOfficeMovies
 
+    private var _weeklyBoxOfficeMovies = MutableLiveData<Response<WeeklyBoxOfficeResponse>>()
+    val weeklyBoxOfficeMovies: LiveData<Response<WeeklyBoxOfficeResponse>> = _weeklyBoxOfficeMovies
+
     fun getDailyBoxOffice(targetDt: String) {
         viewModelScope.launch {
-            _dailyBoxOfficeMovies.value = repository.getDailyBoxOffice(targetDt)
+            kotlin.runCatching { repository.getDailyBoxOffice(targetDt) }
+                .onSuccess {
+                    _dailyBoxOfficeMovies.value = it
+                }
+                .onFailure {
+                    Log.e("MovieViewModel", "getDailyBoxOffice error", it)
+                }
+        }
+    }
+
+    fun getWeeklyBoxOffice(targetDt: String) {
+        viewModelScope.launch {
+            kotlin.runCatching { repository.getWeeklyBoxOffice(targetDt) }
+                .onSuccess {
+                    _weeklyBoxOfficeMovies.value = it
+                }
+                .onFailure {
+                    Log.e("MovieViewModel", "getDailyBoxOffice error", it)
+                }
         }
     }
 }
