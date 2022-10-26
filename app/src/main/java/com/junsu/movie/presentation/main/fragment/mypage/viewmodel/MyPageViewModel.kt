37bab5d891901ee5b983508cbae36560e558d869
Junsu <krpjs0508@gmail.com>
2022-10-27 08:35:33 +0900

    feat :: MyPageViewModel

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/viewmodel/MyPageViewModel.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/viewmodel/MyPageViewModel.kt
@@ -1,4 +1,22 @@
 package com.junsu.movie.presentation.main.fragment.mypage.viewmodel
 
-class MyPageViewModel {
+import MyPageRepository
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.junsu.movie.data.local.getFavoriteMovieDB
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MyPageViewModel(private val myPageRepository: MyPageRepository) : ViewModel() {
+
+    suspend fun getAllFavoriteMovies(context: Context) {
+        viewModelScope.launch(Dispatchers.IO) {
+            kotlin.runCatching {
+                myPageRepository.getAllFavoriteMovies(getFavoriteMovieDB(context))
+            }.onSuccess {
+                
+            }
+        }
+    }
 }
