9c0442875468b0c144833043d8c94bdaa84f7b9b
Junsu <krpjs0508@gmail.com>
2022-10-27 08:41:31 +0900

    feat :: MyPageViewModel in MyPageFragment

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/view/MyPageFragment.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/view/MyPageFragment.kt
@@ -1,6 +1,10 @@
 package com.junsu.movie.presentation.main.fragment.mypage.view
 
+import MyPageRepository
+import androidx.lifecycle.ViewModelProvider
 import com.junsu.movie.presentation.base.BaseFragment
+import com.junsu.movie.presentation.main.fragment.mypage.viewmodel.MyPageViewModel
+import com.junsu.movie.presentation.main.fragment.mypage.viewmodel.MyPageViewModelFactory
 import com.junsu.movieapplication.R
 import com.junsu.movieapplication.databinding.FragmentMypageBinding
 
@@ -8,6 +12,13 @@ class MyPageFragment :
     BaseFragment<FragmentMypageBinding>(
         R.layout.fragment_mypage
     ) {
+
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this,
+            MyPageViewModelFactory(MyPageRepository())
+        )[MyPageViewModel::class.java]
+    }
 
     override fun observeEvent() {}
 }
