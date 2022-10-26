1ea195bbce35ba197f410fd84afd798af5da9b69
Junsu <krpjs0508@gmail.com>
2022-10-26 19:59:13 +0900

    feat :: room database

---
+++ app/src/main/java/com/junsu/movie/data/local/FavoriteMovieDB.kt
@@ -0,0 +1,30 @@
+package com.junsu.movie.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.junsu.movie.data.model.MovieEntity
+
+@Database(entities = [MovieEntity::class], version = 1)
+abstract class FavoriteMovieDB : RoomDatabase() {
+    abstract fun movieDAO(): MovieEntityDAO
+
+    companion object {
+        private var instance: FavoriteMovieDB? = null
+
+        @Synchronized
+        fun getInstance(context: Context): FavoriteMovieDB? {
+            if (instance == null) {
+                synchronized(FavoriteMovieDB::class) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        FavoriteMovieDB::class.java,
+                        "database"
+                    ).build()
+                }
+            }
+            return instance
+        }
+    }
+}
