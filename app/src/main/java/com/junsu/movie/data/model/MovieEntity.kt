dc5cf32c8bf40e8da656eee1a637754b9822fd04
Junsu <krpjs0508@gmail.com>
2022-10-26 18:25:25 +0900

    refactor :: movieEntity

--- app/src/main/java/com/junsu/movie/data/model/MovieEntity.kt
+++ app/src/main/java/com/junsu/movie/data/model/MovieEntity.kt
@@ -4,9 +4,8 @@ import androidx.room.Entity
 
 @Entity(tableName = "MovieEntity")
 data class MovieEntity(
-    var title: String,
-    var createdAt: String,
     var movieInfo: MovieInfo,
+    var createdAt: String,
 ) {
     var id = 0
 }
