01ef97c240a7b6aa250ca06edeea930e47e0e8f8
Junsu <krpjs0508@gmail.com>
2022-10-27 13:28:20 +0900

    feat :: application class

--- app/src/main/java/com/junsu/movie/common/MovieApplication.kt
+++ app/src/main/java/com/junsu/movie/common/MovieApplication.kt
@@ -1,5 +1,14 @@
 package com.junsu.movie.common
 
 import android.app.Application
+import com.junsu.movie.data.local.FavoriteMovieDB
+import com.junsu.movie.data.local.favoriteMovieDB
 
-class MovieApplication : Application() {}
+class MovieApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        favoriteMovieDB = FavoriteMovieDB.getInstance(this)
+    }
+}
