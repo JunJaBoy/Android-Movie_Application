39d103c1716a9e16e21d7437f10fe3557440a0f3
Junsu <krpjs0508@gmail.com>
2022-10-27 11:19:26 +0900

    feat :: OnMovieItemClickListener

--- app/src/main/java/com/junsu/movie/common/OnMovieItemClickListener.kt
+++ app/src/main/java/com/junsu/movie/common/OnMovieItemClickListener.kt
@@ -5,3 +5,7 @@ import android.view.View
 interface OnMovieItemClickListener {
     fun onMovieItemClick(view: View, movieCode: String)
 }
+
+interface OnFavoriteItemClickListener {
+    fun onDeleteFavoriteItemClick(view: View)
+}
