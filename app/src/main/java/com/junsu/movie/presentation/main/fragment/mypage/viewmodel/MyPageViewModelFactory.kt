44ac1c037dc0b2bcfff1c6ba283a4931249153e9
Junsu <krpjs0508@gmail.com>
2022-10-28 14:08:53 +0900

    .11

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/viewmodel/MyPageViewModelFactory.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/viewmodel/MyPageViewModelFactory.kt
@@ -1,9 +1,10 @@
 package com.junsu.movie.presentation.main.fragment.mypage.viewmodel
 
-import com.junsu.movie.data.repository.main.MyPageRepository
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.junsu.movie.data.repository.main.MyPageRepository
 
+@Suppress("UNCHECKED_CAST")
 class MyPageViewModelFactory(private val myPageRepository: MyPageRepository) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
