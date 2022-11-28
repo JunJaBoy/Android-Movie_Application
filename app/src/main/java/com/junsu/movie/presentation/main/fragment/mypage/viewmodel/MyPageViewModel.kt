4174d1224c43f3665ec68d76672085d47fa49713
Junsu <krpjs0508@gmail.com>
2022-11-28 12:28:53 +0900

    dialog_fragment_movie_movie_info

--- app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/viewmodel/MyPageViewModel.kt
+++ app/src/main/java/com/junsu/movie/presentation/main/fragment/mypage/viewmodel/MyPageViewModel.kt
@@ -1,11 +1,11 @@
 package com.junsu.movie.presentation.main.fragment.mypage.viewmodel
 
-import com.junsu.movie.data.repository.main.MyPageRepository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.junsu.movie.data.model.MovieEntity
+import com.junsu.movie.data.repository.main.MyPageRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
